04d0bd8a64797c6f91ce5fef43a12cad16040ec5
wenhao.wu@ucdavis <wnhwu@ucdavis.edu>
2015-05-08 23:32:27 -0700

    Adding the cpp file to the package.

---
+++ BMLGrid/src/BMLGrid.cpp
@@ -0,0 +1,17 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//' Simulator for Biham-Middleton-Levine Traffic Model.
+//' 
+//' The function that actuall runs the Biham-Middleton-Levine Traffic Model from an initial state by a given number of steps.
+//' @param g A BMLGrid class object representing the initial state of the grid.
+//' @param numSteps Number of moves/periods.
+//' @examples
+//' library(BMLGrid)
+//' g = createBMLGrid(r = 100, c = 99, ncars = c(red = 100, blue = 100))
+//' g.out = crunBMLGrid(g, 10000)
+//' plot(g.out)
+// [[Rcpp::export]]
+NumericMatrix crunBMLGrid(NumericMatrix g, int numSteps) {
+   return g;
+}
